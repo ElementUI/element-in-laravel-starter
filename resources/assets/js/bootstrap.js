7449405103283b5ce020d19784fac45f110c8a38
SkyAo <csvwolf@qq.com>
2017-05-09 19:09:21 +0800

    update: delete unused bootstrap-sass

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -9,7 +9,7 @@ window._ = require('lodash');
 
 window.$ = window.jQuery = require('jquery');
 
-require('bootstrap-sass');
+// require('bootstrap-sass');
 
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -1,12 +1,19 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
+    <h1>{{ msg }}</h1>
+    <el-radio class="radio" v-model="radio" label="1">备选项</el-radio>
+    <el-radio class="radio" v-model="radio" label="2">备选项</el-radio>
+    <el-radio class="radio" v-model="radio" label="3">备选项</el-radio>
+
+  </div>
 </template>
 
 <script>
   export default {
     data () {
       return {
-        msg: 'This is a Example~!'
+        msg: 'This is a Example~!',
+        radio: '1'
       }
     }
   }
